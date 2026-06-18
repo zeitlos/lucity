6e91ca2ed63cfeabbc793d58bb27eb54014a496d
Christian <christian@zeitlos.software>
2026-06-18 16:35:04 +0200

    changing the structure of the secret

--- charts/embed.go
+++ charts/embed.go
@@ -3,7 +3,6 @@ package charts
 import "embed"
 
 // LucityApp embeds the lucity-app Helm chart files.
-// This is imported by the packager to inline the chart into GitOps repos.
 //
 //go:embed all:lucity-app
 var LucityApp embed.FS
