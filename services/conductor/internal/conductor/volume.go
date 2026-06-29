7b4c10f7fc55e2698095a27ef11452922ef3db20
Christian <christian@zeitlos.software>
2026-06-29 07:55:26 +0200

    internal volume functions

--- services/conductor/internal/conductor/volume.go
+++ services/conductor/internal/conductor/volume.go
@@ -1,5 +1,18 @@
 package conductor
 
-import "github.com/zeitlos/lucity/services/conductor/internal/platform"
+import (
+	"context"
+
+	"github.com/zeitlos/lucity/services/conductor/internal/platform"
+)
 
 type VolumeID = platform.VolumeID
+type Volume = platform.Volume
+
+func (c *Client) Volumes(ctx context.Context, environment EnvironmentID) ([]Volume, error) {
+	return c.platform.Volumes(ctx, environment)
+}
+
+func (c *Client) Volume(ctx context.Context, id VolumeID) (*Volume, error) {
+	return c.platform.Volume(ctx, id)
+}
